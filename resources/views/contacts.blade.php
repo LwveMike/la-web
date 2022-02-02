ee367a139b4cd41cd87342f1108e1954165d6617
lwvemike <lwvemike@yahoo.com>
2022-02-01 21:35:53 -0800

    first commit

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,7 @@
+@extends('layout', ['title' => 'Contacts'])
+
+@section('content')
+
+<h1>Contacts</h1>
+
+@endsection
---
+++ resources/views/projects.blade.php
@@ -0,0 +1,7 @@
+@extends('layout', ['title' => 'Projects'])
+
+@section('content')
+
+<h1>Projects</h1>
+
+@endsection
